634a03619693126c8fc2116b70068d2f5141793c
Angelique Abacajan <ma_utsey@yahoo.com>
2023-08-13 22:31:36 -0400

    Implemented functionality to generate random quote and functionl share Twitter and Tumbler buttons

--- Build a Random Quote Machine/index.js
+++ Build a Random Quote Machine/index.js
@@ -41,6 +41,53 @@ function getRandomQuote() {
     ];
 }
 
+function getQuote() {
+    let randomQuote = getRandomQuote();
+
+    currentQuote = randomQuote.quote;
+    currentAuthor = randomQuote.author;
+
+    $('#tweet-quote').attr(
+        'href',
+        'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
+        encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
+    );
+
+    $('#tumblr-quote').attr(
+        'href',
+        'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' +
+        encodeURIComponent(currentAuthor) +
+        '&content=' +
+        encodeURIComponent(currentQuote) +
+        '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'
+    );
+
+    $('.quote-text').animate({ opacity: 0 }, 500, function () {
+        $(this).animate({ opacity: 1 }, 500);
+        $('#text').text(randomQuote.quote);
+    });
+
+    $('.quote-author').animate({ opacity: 0 }, 500, function () {
+        $(this).animate({ opacity: 1 }, 500);
+        $('#author').html(randomQuote.author);
+    });
+
+    var color = Math.floor(Math.random() * colors.length);
+    $('html body').animate(
+        {
+        backgroundColor: colors[color],
+        color: colors[color]
+        },
+        1000
+    );
+    $('.button').animate(
+        {
+        backgroundColor: colors[color]
+        },
+        1000
+    );
+}
+
 $(document).ready(function () {
     getQuotes().then(() => {
         getQuote();
