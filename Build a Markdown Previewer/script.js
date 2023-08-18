bb242052e7322da6665ff53a0cb7dbe005d6e4dd
Angelique Abacajan <ma_utsey@yahoo.com>
2023-08-17 23:34:40 -0400

    Implemented initial markdown to render into ui

---
+++ Build a Markdown Previewer/script.js
@@ -0,0 +1,23 @@
+const initialMarkdown = `
+# H1
+    ## H2
+    [title](https://www.example.com)
+    \`code\`
+    \`\`\`
+    {
+      "firstName": "John",
+      "lastName": "Smith",
+      "age": 25
+    }
+    \`\`\`
+
+    - First item
+    - Second item
+    - Third item
+
+    > blockquote
+
+    ![alt text](image.jpg)
+
+    **bold text**
+`
